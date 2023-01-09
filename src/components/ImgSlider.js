5a904072559929a076980eb354d8dbf5692be96c
AnishPowar01 <powaranish74@gmail.com>
2023-01-09 20:18:19 +0530

    Added : Viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -21,6 +21,12 @@ function ImgSlider() {
       <Wrap>
         <img src="/images/slider-badag.jpg" />
       </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" />
+      </Wrap>
     </Carousel>
   );
 }
@@ -29,45 +35,40 @@ export default ImgSlider;
 
 const Carousel = styled(Slider)`
   margin-top: 20px;
-   
-   ul li button
-   {
-    &:before{
-        font-size:10px;
-        color:rgb(150,158,171);
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
     }
-   }
-   
-
-  li.slick-active button:before{
-    color:white;
   }
 
-  .slick-list
-  {
-    overflow:visible;
+  li.slick-active button:before {
+    color: white;
   }
 
-  button{
-    z-index:1;
+  .slick-list {
+    overflow: visible;
   }
 
+  button {
+    z-index: 1;
+  }
 `;
 
 const Wrap = styled.div`
-cursor:pointer;
-img{
-    border:4px solid transparent;
+  cursor: pointer;
+  img {
+    border: 4px solid transparent;
     border-radius: 4px;
-    box-shadow:rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    width:100%;
-    height:100%;
-    transition-duration:300ms;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    width: 100%;
+    height: 100%;
+    transition-duration: 300ms;
 
-    &:hover{
-        border:4px solid rgba(249,249,249,0.8);
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
     }
-
-}
+  }
 `;
